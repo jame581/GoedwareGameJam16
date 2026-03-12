74180acb71a6f32bd2a5a71c2e4cb45046c2e811
Jan Mesarč <jame_581@windowslive.com>
2026-03-12 23:28:54 +0100

    Player hitbox and hurtbox implementation

--- scripts/player/heavy_attack_state.gd
+++ scripts/player/heavy_attack_state.gd
@@ -5,6 +5,7 @@ extends LimboState
 @export var animation_player: AnimationPlayer
 @export var animation_name: String = "heavy_attack"
 @export var damage: int = 3
+@export var hitbox: HitboxComponent
 
 func _enter() -> void:
 	var horizontal_direction = Input.get_axis(&"left", &"right")
@@ -12,7 +13,13 @@ func _enter() -> void:
 		agent.flip_sprite(horizontal_direction)
 
 	agent.velocity.x = 0.0
+	if hitbox:
+		hitbox.damage = damage
+		hitbox.enabled = true
+
 	animation_player.play(animation_name)
 	await animation_player.animation_finished
+	if hitbox:
+		hitbox.enabled = false
 	if is_active():
 		get_root().dispatch(EVENT_FINISHED)
--- scripts/player/light_attack_state.gd
+++ scripts/player/light_attack_state.gd
@@ -5,13 +5,20 @@ extends LimboState
 @export var animation_player: AnimationPlayer
 @export var animation_name: String = "light_attack"
 @export var damage: int = 1
+@export var hitbox: HitboxComponent
 
 func _enter() -> void:
 	var horizontal_direction = Input.get_axis(&"left", &"right")
 	if horizontal_direction != 0.0:
 		agent.flip_sprite(horizontal_direction)
 
+	if hitbox:
+		hitbox.damage = damage
+		hitbox.enabled = true
+
 	animation_player.play(animation_name)
 	await animation_player.animation_finished
+	if hitbox:
+		hitbox.enabled = false
 	if is_active():
 		get_root().dispatch(EVENT_FINISHED)
