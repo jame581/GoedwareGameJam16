b6ff8ebd26fa9683075e217627315b21dadd0c7d
Jan Mesarč <jame_581@windowslive.com>
2026-03-12 22:59:34 +0100

    Main menu reworked

---
+++ scripts/ui/level_main_menu.gd
@@ -0,0 +1,33 @@
+extends Panel
+
+@onready var version_label: Label = %VersionLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var credits_box: Control = %CreditsBox
+
+@onready var quit_button: Button = %QuitButton
+
+var hide_credits = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	version_label.text = "Version: " + Global.get_game_version()
+	if OS.has_feature("web"):
+		quit_button.hide()
+
+
+func _on_quit_button_pressed() -> void:
+	AudioManager.play_button_sound()
+	get_tree().quit()
+
+
+func _on_credits_button_pressed() -> void:
+	AudioManager.play_button_sound()
+	if credits_box.visible:
+		animation_player.play("hide_credits")
+	else:
+		animation_player.play("show_credits")
+
+
+func _on_start_button_pressed() -> void:
+	AudioManager.play_button_sound()
+	SceneChanger.goto_intro()
--- scripts/ui/ui_credits.gd
+++ scripts/ui/ui_credits.gd
@@ -1,5 +1,15 @@
 extends Control
 
+@export var hide_back_button: bool = false
+@export var title_label: String = "Thanks for playing!"
+
+@onready var title_label_node: Label = %TitleLabel
+@onready var back_button: Button = %BackButton
+
+func _ready() -> void:
+	title_label_node.text = title_label
+	back_button.visible = not hide_back_button
+
 func _on_back_button_pressed() -> void:
 	AudioManager.play_button_sound()
 	SceneChanger.goto_main_menu()
